66effb53345b175bcc765e2df5fc415d3e7d4378
desdesevans <156264576+desdesevans@users.noreply.github.com>
2024-02-25 23:56:59 +0000

    Final Edit

---
+++ function.js
@@ -0,0 +1,6 @@
+// Add JavaScript below
+ScrollReveal().reveal('.degree', { delay: 500 });
+ScrollReveal().reveal('.field', { delay: 1500 });
+ScrollReveal().reveal('.flame', { delay: 2500 });
+ScrollReveal().reveal('.screen', { delay: 3500 });
+ScrollReveal().reveal('.stole', { delay: 4500 });
